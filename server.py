6d5235a119efaba485251f07ade0a7c400e01876
fluxTH <fuu.thitat@gmail.com>
2021-04-26 03:22:44 +0700

    Add app development structure

---
+++ server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+import config
+
+app = Flask(config.APP_NAME, template_folder='./frontend')
+
+@app.route("/")
+def root():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    # run debug server
+    app.run(host='127.0.0.1', port=5050, debug=True)

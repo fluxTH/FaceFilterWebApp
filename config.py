57d32d4840b7b6fcadb3cdcde1567b95965ed088
fluxTH <fuu.thitat@gmail.com>
2021-04-28 12:09:45 +0700

    Refactor config paths to absolute

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-from os.path import abspath
+from os.path import abspath, join
 
-APP_NAME = 'ComEngApp WebApp'
+APP_NAME = 'ComEngEss WebApp'
 
 DATABASE_URI = 'sqlite:///database.sqlite3'
 
 PROJECT_PATH = abspath('.')
 
-TEMPLATE_PATH = './frontend'
-STATIC_PATH = './frontend/static'
-ORIGINAL_MEDIA_PATH = './data/original_images'
-PROCESSED_MEDIA_PATH = './data/processed_images'
-FILTER_PATH = './data/filter'
+TEMPLATE_PATH =         join(PROJECT_PATH, 'frontend')
+STATIC_PATH =           join(PROJECT_PATH, 'frontend/static')
+ORIGINAL_MEDIA_PATH =   join(PROJECT_PATH, 'data/original_images')
+PROCESSED_MEDIA_PATH =  join(PROJECT_PATH, 'data/processed_images')
+FILTER_PATH =           join(PROJECT_PATH, 'data/filter')
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])

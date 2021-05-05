950dc8bfdd6ab72e8cd45b09b31ba55320b13735
fluxTH <fuu.thitat@gmail.com>
2021-05-05 12:19:32 +0700

    Timestamp bugfix

--- frontend/static/app.js
+++ frontend/static/app.js
@@ -1,36 +1,31 @@
 function timeDifference(current, previous) {
+  var msPerMinute = 60 * 1000;
+  var msPerHour = msPerMinute * 60;
+  var msPerDay = msPerHour * 24;
+  var msPerMonth = msPerDay * 30;
+  var msPerYear = msPerDay * 365;
 
-    var msPerMinute = 60 * 1000;
-    var msPerHour = msPerMinute * 60;
-    var msPerDay = msPerHour * 24;
-    var msPerMonth = msPerDay * 30;
-    var msPerYear = msPerDay * 365;
+  var elapsed = current - previous;
 
-    var elapsed = current - previous;
-
-    if (elapsed < msPerMinute) {
-         return Math.round(elapsed/1000) + 's';   
-    }
-
-    else if (elapsed < msPerHour) {
-         return Math.round(elapsed/msPerMinute) + 'm';   
-    }
-
-    else if (elapsed < msPerDay ) {
-         return Math.round(elapsed/msPerHour ) + 'h';   
-    }
-
-    else if (elapsed < msPerMonth) {
-        return Math.round(elapsed/msPerDay) + 'd';   
-    }
-
-    else if (elapsed < msPerYear) {
-        return Math.round(elapsed/msPerMonth) + 'mo';   
-    }
-
-    else {
-        return Math.round(elapsed/msPerYear ) + 'y';   
-    }
+  if (elasped < 10 * 1000)
+    return 'just now';
+  
+  if (elapsed < msPerMinute) 
+    return Math.round(elapsed/1000) + 's';
+  
+  if (elapsed < msPerHour) 
+    return Math.round(elapsed/msPerMinute) + 'm';
+  
+  if (elapsed < msPerDay) 
+    return Math.round(elapsed/msPerHour) + 'h';
+  
+  if (elapsed < msPerMonth) 
+    return Math.round(elapsed/msPerDay) + 'd';
+  
+  if (elapsed < msPerYear) 
+    return Math.round(elapsed/msPerMonth) + 'mo';
+  
+  return Math.round(elapsed/msPerYear) + 'y';
 }
 
 function readImageFile(input, cb) {
